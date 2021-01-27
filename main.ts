00690295e995f16ae00a54ced6b1253fdea04f42
LSC862 <LSC862@users.noreply.github.com>
2021-01-27 15:55:00 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -365,6 +365,7 @@ namespace hicbit {
     //% speed1.min=-100 speed1.max=100 
     //% speed2.min=-100 speed2.max=100
     //% speed3.min=-100 speed3.max=100
+    //% inlineInputMode=inline
     export function hicbit_setTripleMotor(port1: hicbit_Port, speed1: number,port2: hicbit_Port, speed2: number, port3:hicbit_Port,speed3:number,Features: hicbit_Features, content: number)
     {
         let direction1:number
@@ -475,6 +476,7 @@ namespace hicbit {
     //% speed2.min=-100 speed2.max=100
     //% speed3.min=-100 speed3.max=100
     //% speed4.min=-100 speed4.max=100
+    //% inlineInputMode=inline
     export function hicbit_set4Motor(port1: hicbit_Port, speed1: number,port2: hicbit_Port, speed2: number, port3:hicbit_Port,speed3:number,port4:hicbit_Port,speed4:number,Features: hicbit_Features, content: number)
     {
         let direction1:number
@@ -850,6 +852,7 @@ namespace hicbit {
     */
     //% weight=97 blockId=hicbit_setCodedmotor block="Set |port %port| motor|angle %angle| speed|speed %speed|  "
     //% angle.min=-360 angle.max=360
+    //% inlineInputMode=inline
     export function hicbit_setCodedmotor(port: hicbit_Coded_motor_Port,angle: number,speed:number) {
         let direction: number = 0;
 	let angle_H, angle_L; 
